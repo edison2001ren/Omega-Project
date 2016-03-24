a710ce37ac01479ababa55b6ff35faf7b9800d18
Liwei Sun <liwei.sun@ask.com>
2016-03-23 17:02:33 -0700

    Recent checkin

--- Practice/src/FindMajNumber.java
+++ Practice/src/FindMajNumber.java
@@ -1,28 +1,27 @@
 /**
  * Created by sunl on 2/24/16.
  */
-public class FindMajorityNumber {
+public class FindMajNumber {
 
     public static int majorityNumber(int[] nums) {
 
         int[] counter = new int[10000];
-        int result = 0;
+        int major = 0;
 
         for (int i = 0; i < nums.length; i++) {
             counter[nums[i]]++;
             if (counter[nums[i]] > (nums.length / 2 )) {
-                result = nums[i];
+                major = nums[i];
             }
         }
-
-        return result;
+        return major;
     }
 
     public static void main(String[] args) {
 
         int[] numList = {111,222,111,111,222,343,90,90,111,111,111,0,111};
 
-        System.out.print(FindMajorityNumber.majorityNumber(numList));
+        System.out.print(FindMajNumber.majorityNumber(numList));
     }
 
 }
---
+++ Practice/src/FindMajNumber2.java
@@ -0,0 +1,5 @@
+/**
+ * Created by sunl on 3/23/16.
+ */
+public class FindMajNumber2 {
+}
