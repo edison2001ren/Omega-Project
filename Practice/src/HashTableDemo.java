92cf2b91b127c30a11bc4a22a4b32111861ccf93
Liwei Sun <liwei.sun@ask.com>
2016-04-05 12:01:31 -0700

    Recent checkin

---
+++ Practice/src/HashTableDemo.java
@@ -0,0 +1,27 @@
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+/**
+ * Created by sunl on 4/5/16.
+ */
+public class HashTableDemo {
+
+    public static void main(String[] args) {
+
+        Enumeration names;
+        String key;
+
+        Hashtable<String, String> ht = new Hashtable<String, String>();
+        ht.put("Key1", "Julie");
+        ht.put("Key2", "Lily");
+        ht.put("Key3", "Edi");
+        ht.put("Key4", "Ethan");
+        ht.put("Key5", "Alexa");
+
+        names = ht.keys();
+        while(names.hasMoreElements()){
+            key = (String)names.nextElement();
+            System.out.println(key + " : "+ ht.get(key));
+        }
+    }
+}
