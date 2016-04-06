75f49fd7b4f85ed6634bd90723badd103027b893
Liwei Sun <liwei.sun@ask.com>
2016-04-06 10:58:00 -0700

    Recent checkin

--- Practice/src/LinkedListDemo.java
+++ Practice/src/LinkedListDemo.java
@@ -20,5 +20,27 @@ public class LinkedListDemo {
 
          /*Display Linked List Content*/
         System.out.println("Linked List Content: " + linkedList);
+
+        /*Add First and Last Element*/
+        linkedList.addFirst("First Item");
+        linkedList.addLast("Last Item");
+        System.out.println("LinkedList Content after addition: " +linkedList);
+
+        /*This is how to get and set Values*/
+        Object firstvar = linkedList.get(0);
+        System.out.println("First element: " +firstvar);
+        linkedList.set(0, "Changed first item");
+        Object firstvar2 = linkedList.get(0);
+        System.out.println("First element after update by set method: " +firstvar2);
+
+        /*Remove first and last element*/
+        linkedList.removeFirst();
+        linkedList.removeLast();
+        System.out.println("LinkedList after deletion of first and last element: " +linkedList);
+
+        /* Add to a Position and remove from a position*/
+        linkedList.add(0, "Newly added item");
+        linkedList.remove(2);
+        System.out.println("Final Content: " +linkedList);
     }
 }
