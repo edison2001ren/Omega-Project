76f724c120d95825f3d0331585db81c681999369
Liwei Sun <liwei.sun@ask.com>
2016-06-07 11:57:58 -0700

    Recent checkin

--- Practice/src/MoveZeros.java
+++ Practice/src/MoveZeros.java
@@ -1,3 +1,5 @@
+import java.util.PriorityQueue;
+
 /**
  * Created by sunl on 6/3/16.
  */
@@ -8,31 +10,28 @@ public class MoveZeros {
      */
     public void moveZeroes(int[] nums) {
 
+        boolean hasZero = false;
+        PriorityQueue<Integer> prq = new PriorityQueue<Integer>();
         for(int i=0; i<nums.length; i++) {
             if(nums[i] == 0) {
-                int pos = i;
-                for(int j=i+1; j<nums.length; i++,j++) {
-                    if(nums[j] != 0) {
-                        nums[i] = nums[j];
-                        nums[j] = 0;
-                        i++;
-                    }
-                    else
-                        continue;
-                }
-                nums[i] = 0;
-                i = pos;
-                nums[i] = 0;
-                i = pos;
+                prq.add(i);
+                hasZero = true;
             }
-            else
+            else if(nums[i] != 0 && hasZero == false) {
                 continue;
+            }
+            else if(nums[i] != 0 && hasZero == true) {
+                nums[prq.poll()] = nums[i];
+                nums[i] = 0;
+                prq.add(i);
+                hasZero = true;
+            }
         }
         // Write your code here
     }
 
     public static void main(String[] args) {
-        int[] inputArray = {0, 0, 1, 0};
+        int[] inputArray = {0, 1, 1, 1, 0, 1};
         for (int c : inputArray) {
             System.out.print(c + " ");
         }
